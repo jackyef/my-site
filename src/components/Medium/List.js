438bd8e43267615c0ec9ffce65515ce88e87549d
Jacky Efendi <jacky.efendi1@gmail.com>
2020-01-14 01:37:19 +0700

    feat: added carousel component

--- src/components/Medium/List.js
+++ src/components/Medium/List.js
@@ -1,39 +1,22 @@
 import React from 'react';
 import MediumPostCard from './PostCard';
-import { ThemeContext } from '../../layouts';
+import Carousel from '../Carousel';
 import stories from './medium-stories.json';
 
 const MediumList = () => {
   return (
-    <ThemeContext.Consumer>
-      {theme => (
-        <React.Fragment>
-          <section>
-            {stories.map(s => (
-              <MediumPostCard
-                key={s.title}
-                title={s.title}
-                coverImage={s.coverImage}
-                excerpt={s.excerpt}
-                url={s.url}
-                timeToRead={s.timeToRead}
-              />
-            ))}
-          </section>
-
-          <style jsx>{`
-            section {
-              display: block;
-              padding: 0 ${theme.space.s};
-              white-space: nowrap;
-              overflow-x: scroll;
-              scroll-snap-type: x mandatory;
-              vertical-align: top;
-            }
-          `}</style>
-        </React.Fragment>
-      )}
-    </ThemeContext.Consumer>
+    <Carousel>
+      {stories.map(s => (
+        <MediumPostCard
+          key={s.title}
+          title={s.title}
+          coverImage={s.coverImage}
+          excerpt={s.excerpt}
+          url={s.url}
+          timeToRead={s.timeToRead}
+        />
+      ))}
+    </Carousel>
   );
 };
 
