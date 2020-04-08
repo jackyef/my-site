9915182fc19b8ddff4c3e8009b9ab843eebce07a
Jacky Efendi <jacky.efendi1@gmail.com>
2020-04-08 21:49:28 +0700

    improvement: use preact

--- gatsby-config.js
+++ gatsby-config.js
@@ -48,6 +48,7 @@ module.exports = {
     // },
   },
   plugins: [
+    'gatsby-plugin-preact',
     `gatsby-plugin-styled-jsx`, // the plugin's code is inserted directly to gatsby-node.js and gatsby-ssr.js files
     `gatsby-plugin-styled-jsx-postcss`, // as above
     {
