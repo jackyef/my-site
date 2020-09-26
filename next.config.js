3c9dae999acb3254673f6688b7399038ccc8d2e6
Jacky Efendi <jacky.efendi1@gmail.com>
2020-09-26 16:15:16 +0700

    fix: syntax highlighting css was purged

--- next.config.js
+++ next.config.js
@@ -9,34 +9,8 @@ const { createLoader } = require('simple-functional-loader');
 const rehypePrism = require('@mapbox/rehype-prism');
 const visit = require('unist-util-visit');
 
+const tokenClassNames = require('./code-highlighter-token.js')
 const { flowRight } = require('./utils/flow.js');
-
-// needed to map token to tailwind classes
-const tokenClassNames = {
-  tag: 'text-code-red',
-  'attr-name': 'text-code-yellow',
-  'attr-value': 'text-code-green',
-  deleted: 'text-code-red',
-  inserted: 'text-code-green',
-  punctuation: 'text-code-white',
-  'template-punctuation': 'text-code-green',
-  keyword: 'text-code-purple',
-  string: 'text-code-green',
-  'template-string': 'text-code-green',
-  function: 'text-code-blue',
-  boolean: 'text-code-red',
-  comment: 'text-gray-400 italic',
-  property: 'text-code-teal',
-  'property-access': 'text-code-teal',
-  dom: 'text-code-blue',
-  'method': 'text-code-teal',
-  class: 'text-code-yellow',
-  color: 'text-code-purple',
-  'function-variable': 'text-code-blue', // foo`something`; Here, foo is a function-variable
-  'variable': 'text-code-blue', // foo`something`; Here, foo is a function-variable
-  'interpolation-punctuation': 'text-code-teal', 
-  'interpolation': 'text-code-red', 
-};
 
 const conf = {
   target: 'serverless',
