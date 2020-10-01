e0f10da7c8e1521e5fea10348625043701ce7161
Jacky Efendi <jacky.efendi1@gmail.com>
2020-10-01 19:13:55 +0700

    feat: add webmention replies list under blog post (#30)
    * feat: add webmention replies list under blog post
    
    * chore: remove logs
    
    * fix: revert enabling animation

--- src/components/Webmention/index.tsx
+++ src/components/Webmention/index.tsx
@@ -1,16 +1,23 @@
+import { useState } from 'react';
+
 import { PostMeta } from '@/blog/getAllPostPreviews';
 import { useRouter } from 'next/router';
 import { publicUrl } from '../Seo/PageMetaTags';
 import { TwitterShare } from '../Social/TwitterShare';
 import { useWebmention } from './hooks/useWebmention';
+import { ExternalLink } from '../Typography/ExternalLink';
+import tinytime from 'tinytime';
 
 interface Props {
   url: string;
   meta: PostMeta;
 }
 
+const repliesTimeTemplate = tinytime('{DD} {MM} {YYYY} - {h}:{mm} {a}');
+
 const WebmentionWidget = ({ url, meta }: Props) => {
   const { isLoading, isError, data, refetch } = useWebmention(url);
+  const [showReplies, setShowReplies] = useState(false);
   const router = useRouter();
 
   const content = (() => {
@@ -52,23 +59,81 @@ const WebmentionWidget = ({ url, meta }: Props) => {
       const otherAuthorsCount = authors.length - _authors.length;
 
       return (
-        <div className="flex flex-col md:flex-row items-start md:items-center space-y-4 md:space-y-0">
-          <div className="flex space-x-3 items-center">
-            <div>❤️ {likes.length}</div>
-            <div>🔄 {reposts.length}</div>
-            <div>💬 {discussions.length}</div>
-          </div>
-          <div className="pl-0 md:pl-6">
-            <div className="flex -space-x-2">
-              {authorImages}
-              {otherAuthorsCount > 0 ? (
-                <div className="flex items-center justify-center text-sm pl-4">
-                  and {otherAuthorsCount} others.
-                </div>
-              ) : null}
+        <>
+          <div className="flex flex-col md:flex-row items-start md:items-center space-y-4 md:space-y-0">
+            <div className="flex space-x-3 items-center">
+              <div className="flex space-x-1">
+                <span>❤️</span> <span>{likes.length}</span>
+              </div>
+              <div className="flex space-x-1">
+                <span>🔄</span> <span>{reposts.length}</span>
+              </div>
+              <div className="flex space-x-1">
+                <span>💬</span> <span>{discussions.length}</span>
+                <button
+                  onClick={() => setShowReplies((prev) => !prev)}
+                  className="hover:underline"
+                >
+                  (show all)
+                </button>
+              </div>
+            </div>
+            <div className="pl-0 md:pl-6">
+              <div className="flex -space-x-2">
+                {authorImages}
+                {otherAuthorsCount > 0 ? (
+                  <div className="flex items-center justify-center text-sm pl-4">
+                    and {otherAuthorsCount} others.
+                  </div>
+                ) : null}
+              </div>
             </div>
           </div>
-        </div>
+
+          <TwitterShare
+            text={`${meta.title} ${publicUrl}${router.pathname} via @jackyef__`}
+          >
+            Discuss on Twitter
+          </TwitterShare>
+
+          {showReplies ? (
+            <div className="flex flex-col mt-4 space-y-4">
+              {discussions.map((discussion: any) => {
+                const author = discussion.author || {};
+
+                return (
+                  <div
+                    key={discussion['wm-id']}
+                    className="p-4 md:p-6 rounded-lg shadow-md"
+                  >
+                    <div className="flex flex-col space-y-2">
+                      <div>
+                        <img
+                          className="inline rounded-full w-8 h-8 mr-2"
+                          loading="lazy"
+                          width={48}
+                          height={48}
+                          src={author.photo}
+                          alt={author.name}
+                        />
+                        <ExternalLink href={author.url}>
+                          <span className="font-bold">{author.name}</span>
+                        </ExternalLink>
+                        <span className="ml-1 text-sm text-gray-500">
+                          &middot;{' '}
+                          {repliesTimeTemplate.render(
+                            new Date(discussion.published),
+                          )}
+                        </span>
+                      </div>
+                      <p className="text-gray-900">{discussion.content.text}</p>
+                    </div>
+                  </div>
+                );
+              })}
+            </div>
+          ) : null}
+        </>
       );
     }
   })();
@@ -83,12 +148,6 @@ const WebmentionWidget = ({ url, meta }: Props) => {
           {content}
         </div>
       </div>
-
-      <TwitterShare
-        text={`${meta.title} ${publicUrl}${router.pathname} via @jackyef__`}
-      >
-        Discuss on Twitter
-      </TwitterShare>
     </>
   );
 };
