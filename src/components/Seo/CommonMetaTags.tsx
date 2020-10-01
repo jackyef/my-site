6b06f27333c684b02d1c0fbc6864e02689ba0129
Jacky Efendi <jacky.efendi1@gmail.com>
2020-10-01 21:16:31 +0700

    fix: canonical url was all pointing to homepage

--- src/components/Seo/CommonMetaTags.tsx
+++ src/components/Seo/CommonMetaTags.tsx
@@ -24,7 +24,6 @@ export const CommonMetaTags = () => {
       <meta name="mobile-web-app-capable" content="yes" />
       {/* <meta ref={themeMetaRef} name="theme-color" content={colorMode === 'light' ? '#fff' : '#1A202C'} /> */}
       <meta ref={themeMetaRef} name="theme-color" content={'#1A202C'} />
-      <link rel="canonical" href={publicUrl} />
       <link rel="apple-touch-icon" sizes="57x57" href="/apple-icon-57x57.png" />
       <link rel="apple-touch-icon" sizes="60x60" href="/apple-icon-60x60.png" />
       <link rel="apple-touch-icon" sizes="72x72" href="/apple-icon-72x72.png" />
