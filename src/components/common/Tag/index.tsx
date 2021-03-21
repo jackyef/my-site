db9748bc10dcd9fe3b01d7786d1e3f0bb2849d6e
Jacky Efendi <jacky.efendi1@gmail.com>
2021-03-21 20:08:37 +0700

    improvement: Add outline and fill variant for Tag component

--- src/components/common/Tag/index.tsx
+++ src/components/common/Tag/index.tsx
@@ -1,10 +1,26 @@
 import React from 'react';
 
-const Tag: React.FC = ({ children }) => {
+interface Props {
+  variant?: 'outline' | 'fill'
+  as?: keyof JSX.IntrinsicElements | React.ComponentType
+}
+
+const variantClassesMap: Record<Required<Props>['variant'], string> = {
+  outline: 'bg-transparent',
+  fill: 'bg-blue-500 text-white'
+}
+
+const Tag: React.FC<Props> = ({ variant = 'outline', as = 'span', children, ...props }) => {
+  const variantClasses = variantClassesMap[variant]
+  const Comp = as;
+
   return (
-    <span className="inline-block rounded-lg text-blue-500 border-blue-500 border-solid border py-0.5 px-2 mr-2 mb-2 text-sm">
+    <Comp
+      className={`${variantClasses} inline-block rounded-lg text-theme-text border-blue-500 border-solid border py-0.5 px-2 mr-2 mb-2 text-sm`}
+      {...props}
+    >
       {children}
-    </span>
+    </Comp>
   );
 };
 
