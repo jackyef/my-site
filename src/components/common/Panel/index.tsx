63bcf520a234ffa43a781aca42b90624ac59c179
jackyef <jacky.efendi1@gmail.com>
2021-10-19 22:33:13 +0700

    improvement: Adjust Panel paddings

--- src/components/common/Panel/index.tsx
+++ src/components/common/Panel/index.tsx
@@ -17,7 +17,9 @@ export const Panel: React.FC<Props> = ({ type = 'warning', title = '', children 
   const bgClass = bgMap[type];
 
   return (
-    <div className={`rounded-lg my-8 p-4 ${bgClass}`}>
+    // Different margins for x and y axis to account for the vertical spacing
+    // that exists in text elements
+    <div className={`rounded-lg my-8 px-5 pt-4 pb-1 ${bgClass}`}>
       <div className="font-bold text-xl mb-2">{title}</div>
       {children}
     </div>
