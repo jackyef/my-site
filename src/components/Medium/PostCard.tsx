7f6255a30130bccc1fdae99584383dd349964a1b
jackyef <jacky.efendi1@gmail.com>
2021-10-28 14:10:22 +0700

    fix: Disable eslint-plugin-next for now

--- src/components/Medium/PostCard.tsx
+++ src/components/Medium/PostCard.tsx
@@ -28,7 +28,6 @@ const MediumPostCard = ({
         }}
       >
         {/* The cover images are hosted on Medium */}
-        {/* eslint-disable-next-line @next/next/no-img-element */}
         <img
           loading="lazy"
           src={coverImage}
