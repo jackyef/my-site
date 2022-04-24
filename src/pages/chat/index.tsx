4cf65fc8b697ce48bc995d35f64ac232a04cf522
jackyef <jacky.efendi1@gmail.com>
2022-04-24 22:12:35 +0700

    Chat redirect method, allow OG image to be used

---
+++ src/pages/chat/index.tsx
@@ -0,0 +1,26 @@
+import { PageMetaTags } from '@/components/Seo/PageMetaTags';
+import Head from 'next/head';
+
+const ChatPage = () => {
+  return (
+    <>
+      <PageMetaTags
+        title="30-min casual chat 💬"
+        description="Schedule a casual chat about tech/career related stuff"
+        image="https://jackyef-og-img.vercel.app/**30-min%20casual%20chat**%20%F0%9F%92%AC.png?fontSize=150px"
+      />
+      <Head>
+        <meta
+          httpEquiv="refresh"
+          content="0; url=https://cal.com/jackyef/casual-chat"
+        />
+      </Head>
+
+      <div className="text-center py-24">
+        <h1 className="text-3xl md:text-6xl">Redirecting...</h1>
+      </div>
+    </>
+  );
+};
+
+export default ChatPage;
