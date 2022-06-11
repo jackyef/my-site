34ca336788df637210cf921d2b012be853be006d
jackyef <jacky.efendi1@gmail.com>
2022-06-11 17:34:48 +0700

    Update Tag focus style

--- src/components/Tag/Tag.tsx
+++ src/components/Tag/Tag.tsx
@@ -22,6 +22,16 @@ export const Tag: React.FC<Props> = ({ children, variant }) => {
       var(--h-${variant}) var(--s-${variant}) var(--l-${variant})
     );
     border-radius: 0.5rem;
+
+    /* override focus style when children is focusable */
+    &:focus-within {
+      box-shadow: 0 0 0 3px currentColor;
+    }
+
+    &:focus-within *:focus {
+      box-shadow: none !important;
+      outline: none !important;
+    }
   `;
 
   return (
