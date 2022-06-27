6d93088913317b505f4c8c0b9b8654d456d9752b
jackyef <jacky.efendi1@gmail.com>
2022-06-27 16:23:47 +0700

    Replace old implementation

---
+++ src/components/Analytics/Analytics.tsx
@@ -0,0 +1,28 @@
+import Head from 'next/head';
+import Script from 'next/script';
+
+import { isProd } from '@/utils/constants';
+
+export const Analytics = () => {
+  return isProd ? (
+    <>
+      <Script
+        strategy="afterInteractive"
+        src="https://www.googletagmanager.com/gtag/js?id=G-6CK1QFKMHJ"
+      ></Script>
+      <Head>
+        {/* Global site tag (gtag.js) - Google Analytics */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+
+              gtag('config', 'G-6CK1QFKMHJ');`,
+          }}
+        ></script>
+      </Head>
+    </>
+  ) : null;
+};
--- types/window.d.ts
+++ types/window.d.ts
@@ -3,5 +3,6 @@ import { Theme } from '@/components/Theme/ThemeProvider';
 declare global {
   interface Window {
     __themeBinding: (theme: Theme) => void;
+    gtag: (...args: any) => void;
   }
 }
