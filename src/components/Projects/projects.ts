8ae7302bdc5befb488402f88ad6907306e02d6a8
jackyef <jacky.efendi1@gmail.com>
2022-09-10 12:51:28 +0700

    Update project list

--- src/components/Projects/projects.ts
+++ src/components/Projects/projects.ts
@@ -7,6 +7,18 @@ export type Project = {
 
 export const projects: Project[] = [
   {
+    name: 'ProfileOverflow',
+    url: 'https://profile-overflow.vercel.app',
+    repo: 'https://github.com/jackyef/profile-overflow',
+    coverImage: 'https://profile-overflow.vercel.app/api/og/users/4662933',
+  },
+  {
+    name: 'Kotla',
+    url: 'https://kotla.vercel.app',
+    repo: 'https://github.com/jackyef/kotla',
+    coverImage: 'https://kotla.vercel.app/og.png',
+  },
+  {
     name: 'Indonesian remote-friendly companies 🇮🇩',
     url: 'https://id-wfa-companies.vercel.app',
     repo: 'https://github.com/jackyef/id-wfa',
