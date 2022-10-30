93348927c8a3185c4f386136cc363d38ad40249e
jackyef <jacky.efendi1@gmail.com>
2022-10-30 15:51:40 +0700

    Clean up

--- next.config.js
+++ next.config.js
@@ -12,24 +12,6 @@ const conf = {
   images: {
     formats: ['image/avif', 'image/webp'],
   },
-
-  // webpack(config, options) {
-  //   // https://github.com/vercel/next.js/issues/36896#issuecomment-1126202598
-  //   if (!options.dev && options.isServer && options.nextRuntime === 'nodejs') {
-  //     const originalEntry = config.entry;
-
-  //     config.entry = async () => {
-  //       const entries = { ...(await originalEntry()) };
-
-  //       // we want to build this script as well, and run it on build to generate feed.xml file
-  //       entries['scripts/build-rss'] = './scripts/build-rss.js';
-
-  //       return entries;
-  //     };
-  //   }
-
-  //   return config;
-  // },
 };
 
 module.exports = flowRight(withBundleAnalyzer)(conf);
