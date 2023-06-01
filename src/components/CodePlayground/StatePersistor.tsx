bcd41565a54c75edf05b4d03790631facb07d041
jackyef <jacky.efendi1@gmail.com>
2023-06-01 21:31:00 +0700

    Fix StatePersistor logic

--- src/components/CodePlayground/StatePersistor.tsx
+++ src/components/CodePlayground/StatePersistor.tsx
@@ -1,16 +1,20 @@
-import { useActiveCode } from '@codesandbox/sandpack-react';
+import { useActiveCode, useSandpack } from '@codesandbox/sandpack-react';
 import { useEffect } from 'react';
 import * as LZString from 'lz-string';
 
 export const StatePersistor = () => {
+  const { sandpack } = useSandpack();
+  const { activeFile } = sandpack;
   const { code } = useActiveCode();
 
   useEffect(() => {
-    // Update the URL with the current code
-    window.location.hash = `#code=${LZString.compressToEncodedURIComponent(
-      code,
-    )}`;
-  }, [code]);
+    if (activeFile === '/App.tsx') {
+      // Update the URL with the current code
+      window.location.hash = `#code=${LZString.compressToEncodedURIComponent(
+        code,
+      )}`;
+    }
+  }, [code, activeFile]);
 
   return null;
 };
