1e28912fc2ff52c06c36e419147937e17d0de1be
Jacky Efendi <jacky.efendi1@gmail.com>
2024-03-03 17:16:47 +0700

    Set up relative-ci (#110)
    * Set up relative-ci
    
    * Simplify config

--- next.config.js
+++ next.config.js
@@ -1,3 +1,6 @@
+const { StatsWriterPlugin } = require('webpack-stats-plugin');
+const filterWebpackStats =
+  require('@bundle-stats/plugin-webpack-filter').default;
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 });
@@ -11,6 +14,30 @@ const conf = {
   pageExtensions: ['ts', 'tsx'],
   images: {
     formats: ['image/avif', 'image/webp'],
+  },
+  webpack: (config, options) => {
+    const { dev, isServer } = options;
+
+    // Output webpack stats JSON file only for
+    // client-side/production build
+    if (!dev && !isServer) {
+      config.plugins.push(
+        new StatsWriterPlugin({
+          filename: '../webpack-stats.json',
+          stats: {
+            assets: true,
+            chunks: true,
+            modules: true,
+          },
+          transform: (webpackStats) => {
+            const filteredSource = filterWebpackStats(webpackStats);
+            return JSON.stringify(filteredSource);
+          },
+        }),
+      );
+    }
+
+    return config;
   },
 };
 
