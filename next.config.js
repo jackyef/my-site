4f443efa2c86928c4003306d78ccc77460d87093
jackyef <jacky.efendi1@gmail.com>
2024-03-03 15:46:27 +0700

    Set up relative-ci

--- next.config.js
+++ next.config.js
@@ -1,3 +1,5 @@
+const { StatsWriterPlugin } = require('webpack-stats-plugin');
+const { RelativeCiAgentWebpackPlugin } = require('@relative-ci/agent');
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 });
@@ -11,6 +13,31 @@ const conf = {
   pageExtensions: ['ts', 'tsx'],
   images: {
     formats: ['image/avif', 'image/webp'],
+  },
+  webpack: function (config, options) {
+    const { dev, isServer } = options;
+
+    if (!dev && !isServer) {
+      config.plugins.push(
+        new RelativeCiAgentWebpackPlugin({
+          stats: { excludeAssets: [/stats.json/] },
+        }),
+      );
+      config.plugins.push(
+        new StatsWriterPlugin({
+          filename: 'stats.json',
+          stats: {
+            context: './', // optional, will improve readability of the paths
+            assets: true,
+            entrypoints: true,
+            chunks: true,
+            modules: true,
+          },
+        }),
+      );
+    }
+
+    return config;
   },
 };
 
