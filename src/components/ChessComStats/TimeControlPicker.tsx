c66dbd760b57bc054d7ee357bb5733144cba8f93
jackyef <jacky.efendi1@gmail.com>
2024-09-19 00:52:43 +0700

    Fix build

--- src/components/ChessComStats/TimeControlPicker.tsx
+++ src/components/ChessComStats/TimeControlPicker.tsx
@@ -1,4 +1,4 @@
-import { AnimatePresence, motion } from 'framer-motion';
+import { motion } from 'framer-motion';
 import { css } from 'goober';
 
 import { ChessComTimeControl } from 'types/chesscom';
