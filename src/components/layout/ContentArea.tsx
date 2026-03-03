c9f223b594c70f426b08ceb9d130f9f37498fa2c
jackyef <jacky.efendi1@gmail.com>
2026-03-03 23:03:46 +0700

    Tweak scrollbar

--- src/components/layout/ContentArea.tsx
+++ src/components/layout/ContentArea.tsx
@@ -11,6 +11,7 @@ export function ContentArea({ children }: ContentAreaProps) {
       style={{
         flex: 1,
         overflowY: 'auto',
+        scrollbarWidth: 'thin',
         minHeight: '100vh',
         display: 'flex',
         flexDirection: 'column',
