164abd02db55cfa556f4501580cab3e3c8bc3c0e
jackyef <jacky.efendi1@gmail.com>
2026-03-11 21:18:00 +0700

    Fix featured logic

--- src/blog/featured.ts
+++ src/blog/featured.ts
@@ -1,27 +1,28 @@
-import type { WritingItem } from './types';
+import type { WritingItem } from "./types";
 
 const FEATURED_TITLES = [
-  '1-billion row challenge with Node.js',
-  'Web security implications of 3rd party resources',
-  'Writing Your Own CSS-in-JS Library',
-  'Building 60 FPS QR Scanner for the Mobile Web',
-  'Getting Content Painted under 2 seconds on the Mobile Web',
+  "1-billion row challenge with Node.js",
+  "Web security implications of 3rd party resources",
+  "Writing Your Own CSS-in-JS Library",
+  "Building 60 FPS QR Scanner for the Mobile Web",
+  "Getting Content Painted under 2 seconds on the Mobile Web",
 ];
 
 export function getFeaturedWritings(allWritings: WritingItem[]): WritingItem[] {
-  const featured: WritingItem[] = []
+  const featured: WritingItem[] = [];
   featured.unshift(allWritings[0] as WritingItem); // Always include the most recent writing as the first item
 
   featured[0].isLatest = true; // Mark the most recent writing as the latest
 
-  FEATURED_TITLES.forEach((title, index) => {
-    if (index === 0) return; // Skip the most recent writing since it's already included
+  FEATURED_TITLES.forEach((title) => {
     const writing = allWritings.find((w) => w.title.startsWith(title));
+
+    if (featured[0].title === writing?.title) return; // Skip if the writing is already included as the most recent one
 
     if (writing) {
       featured.push(writing);
     }
-  })
+  });
 
   return featured;
 }
